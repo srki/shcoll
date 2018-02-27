505923e97140fdeec21fccfa355b9f483d3e899b
Srđan Milaković <srdjan@rice.edu>
2018-02-27 15:37:19 -0600

    struct/typedef collapsed into on declaration

--- src/util/trees.h
+++ src/util/trees.h
@@ -3,25 +3,21 @@
 
 #include <limits.h>
 
-struct _node_info_binomial {
+typedef struct {
     int parent;
     int children_num;
     int children[sizeof(int) * CHAR_BIT];
-};
-
-typedef struct _node_info_binomial node_info_binomial_t;
+} node_info_binomial_t;
 
 void get_node_info_binomial(int tree_size, int node, node_info_binomial_t *node_info);
 
 
-struct _node_info_complete {
+typedef struct _node_info_complete {
     int parent;
     int children_num;
     int children_begin;
     int children_end;
-};
-
-typedef struct _node_info_complete node_info_complete_t;
+} node_info_complete_t;
 
 void get_node_info_complete(int tree_size, int tree_degree, int node, node_info_complete_t *node_info);
 
