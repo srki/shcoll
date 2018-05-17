0e69703de33799602f4a29bc76ace962eef6b158
Srđan Milaković <srdjan@rice.edu>
2018-05-17 11:03:04 -0600

    Fixed synchronization bug in broadcast test. Bug: dst array was not ready

--- test/broadcast_test.c
+++ test/broadcast_test.c
@@ -14,6 +14,8 @@ unsigned long long current_time_ns() {
     return (((unsigned long long) t.tv_nsec)) + s;
 }
 
+#define gprintf(...) fprintf(stderr, __VA_ARGS__)
+
 typedef void (*broadcast_impl)(void *, const void *, size_t, int, int, int, int, long *);
 
 double test_broadcast(broadcast_impl broadcast, int iterations, size_t count) {
@@ -30,8 +32,6 @@ double test_broadcast(broadcast_impl broadcast, int iterations, size_t count) {
         src[i] = (uint32_t) i;
     }
 
-    int root = shmem_n_pes() - 1;
-
     shmem_barrier_all();
     unsigned long long start = current_time_ns();
 
@@ -40,8 +40,9 @@ double test_broadcast(broadcast_impl broadcast, int iterations, size_t count) {
         memset(dst, 0, count * sizeof(uint32_t));
         #endif
 
-        broadcast(dst, src, count, root, 0, 0, shmem_n_pes(), pSync);
         shmem_sync_all();
+        int root = i % shmem_n_pes();
+        broadcast(dst, src, count, root, 0, 0, shmem_n_pes(), pSync);
 
         #ifdef VERIFY
         if (shmem_my_pe() != root && memcmp(dst, src, count * sizeof(uint32_t)) != 0) {
@@ -65,21 +66,31 @@ double test_broadcast(broadcast_impl broadcast, int iterations, size_t count) {
 
 
 int main(int argc, char *argv[]) {
-    int iterations = 10;
+    int iterations = 100;
     size_t count = 8;
 
     shmem_init();
 
     if (shmem_my_pe() == 0) {
-        printf("PEs: %d\n", shmem_n_pes());
+        gprintf("PEs: %d\n", shmem_n_pes());
     }
 
-    printf("shmem: %lf\n", test_broadcast(shmem_broadcast32, iterations, count));
-    printf("Linear: %lf\n", test_broadcast(shcoll_linear_broadcast32, iterations, count));
-    printf("Binomial: %lf\n", test_broadcast(shcoll_binomial_tree_broadcast32, iterations, count));
-    for (int degree = 2; degree <= 8; degree *= 2) {
-        shcoll_set_broadcast_tree_degree(degree);
-        printf("Complete %d: %lf\n", degree, test_broadcast(shcoll_complete_tree_broadcast32, iterations, count));
+    if (shmem_my_pe()) {
+        gprintf("shmem: %lf\n", test_broadcast(shmem_broadcast32, iterations, count));
+        gprintf("Linear: %lf\n", test_broadcast(shcoll_linear_broadcast32, iterations, count));
+        gprintf("Binomial: %lf\n", test_broadcast(shcoll_binomial_tree_broadcast32, iterations, count));
+        for (int degree = 2; degree <= 8; degree *= 2) {
+            shcoll_set_broadcast_tree_degree(degree);
+            gprintf("Complete %d: %lf\n", degree, test_broadcast(shcoll_complete_tree_broadcast32, iterations, count));
+        }
+    } else {
+        test_broadcast(shmem_broadcast32, iterations, count);
+        test_broadcast(shcoll_linear_broadcast32, iterations, count);
+        test_broadcast(shcoll_binomial_tree_broadcast32, iterations, count);
+        for (int degree = 2; degree <= 8; degree *= 2) {
+            shcoll_set_broadcast_tree_degree(degree);
+            test_broadcast(shcoll_complete_tree_broadcast32, iterations, count);
+        }
     }
 
     shmem_finalize();
