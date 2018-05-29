aa226ea2665c4945d866c55bd3d57ad366c7936e
Srđan Milaković <srdjan@rice.edu>
2018-05-29 17:27:48 -0600

    Added PLII debug macro.

--- test/util/debug.h
+++ test/util/debug.h
@@ -11,5 +11,6 @@
 #define gprintf(...) fprintf(stderr, __VA_ARGS__)
 #define PL gprintf("[%d]:%d\n", shmem_my_pe(), __LINE__);
 #define PLI(A) gprintf("[%d]:%d %d\n", shmem_my_pe(), __LINE__, (A));
+#define PLII(A, B) gprintf("[%d]:%d %d %d\n", shmem_my_pe(), __LINE__, (A), (B));
 
 #endif //OPENSHMEM_COLLECTIVE_ROUTINES_DEBUG_H
