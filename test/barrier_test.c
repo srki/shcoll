cfa08191eaedfe61e75ee937171e44f28efe56ae
Srđan Milaković <srdjan@rice.edu>
2018-06-05 11:53:55 -0600

    Barrier-test: minor updates

--- test/barrier_test.c
+++ test/barrier_test.c
@@ -20,17 +20,18 @@ double test_barrier(barrier_impl barrier, int iterations, int log2stride,
         pSync[i] = SYNC_VALUE;
     }
     shmem_barrier_all();
+    int npes = shmem_n_pes();
 
     #ifdef WARMUP
     for (int i = 0; i < (iterations / 10); i++) {
-        barrier(0, log2stride, shmem_n_pes() >> log2stride, pSync);
+        barrier(0, log2stride, npes >> log2stride, pSync);
     }
     #endif
 
     time_ns_t start = current_time_ns();
 
     for (int i = 0; i < iterations; i++) {
-        barrier(0, log2stride, shmem_n_pes() >> log2stride, pSync);
+        barrier(0, log2stride, npes >> log2stride, pSync);
     }
 
     shmem_barrier_all();
@@ -47,9 +48,10 @@ int main(int argc, char **argv) {
     int logPE_stride = 0;
 
     shmem_init();
+    int me = shmem_my_pe();
     int npes = shmem_n_pes();
 
-    if (shmem_my_pe() == 0) {
+    if (me == 0) {
         gprintf("PEs: %d\n", npes);
     }
 
@@ -59,18 +61,17 @@ int main(int argc, char **argv) {
 
     for (int degree = 2; degree <= 32; degree *= 2) {
         shcoll_set_tree_degree(degree);
-        if (shmem_my_pe() == 0) gprintf("%2d-", degree);
+        if (me == 0) gprintf("%2d-", degree);
         RUN(barrier, complete_tree, iterations, logPE_stride, SHCOLL_SYNC_VALUE, SHCOLL_BARRIER_SYNC_SIZE);
     }
 
     for (int k = 2; k <= 32; k *= 2) {
         shcoll_set_knomial_tree_radix_barrier(k);
-        if (shmem_my_pe() == 0) gprintf("%2d-", k);
+        if (me == 0) gprintf("%2d-", k);
         RUN(barrier, knomial_tree, iterations, logPE_stride, SHCOLL_SYNC_VALUE, SHCOLL_BARRIER_SYNC_SIZE);
     }
 
     RUNC(npes <= 16 * 24, barrier, linear, iterations, logPE_stride, SHCOLL_SYNC_VALUE, SHCOLL_BARRIER_SYNC_SIZE);
 
     shmem_finalize();
-    return 0;
 }
